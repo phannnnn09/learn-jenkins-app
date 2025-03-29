bcb335f87298d093348ba764bfc7d9e27a911cdb
phannnnn09 <phanthiras64@nu.ac.th>
2025-03-29 16:29:07 +0700

    update jenkinsfile

--- netlify/functions/quote.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/quote.js
-
-exports.handler = async function(event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ quote: "Jenkins is deploying me!" })
-    };
-  };
-  
